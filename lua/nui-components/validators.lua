c627111a658b458118ebc7f6cb32f69505751dab
Will Hopkins <willothyh@gmail.com>
2024-03-28 16:54:54 -0700

    docs: document validator combinators
    refactor!: soft-deprecate `compose` combinator

--- lua/nui-components/validators.lua
+++ lua/nui-components/validators.lua
@@ -1,11 +1,13 @@
 local M = {}
 
+---@type fun(min: integer): fun(arg: string): boolean
 function M.min_length(min)
   return function(arg)
     return #arg >= min
   end
 end
 
+---@type fun(arg: string): boolean
 M.is_not_empty = M.min_length(1)
 
 function M.max_length(max)
@@ -14,32 +16,21 @@ function M.max_length(max)
   end
 end
 
+---@type fun(pattern: string): fun(arg: string): boolean
 function M.contains(pattern)
   return function(arg)
-    return string.find(arg, pattern)
+    return string.find(arg, pattern) ~= nil
   end
 end
 
+---@type fun(value: any): fun(arg: any): boolean
 function M.equals(value)
   return function(arg)
     return vim.deep_equal(value, arg)
   end
 end
 
-function M.compose(...)
-  local tbl = { ... }
-
-  return function(value)
-    for _, fn in ipairs(tbl) do
-      if not fn(value) then
-        return false
-      end
-    end
-
-    return true
-  end
-end
-
+---@type fun(...: fun(arg: any): boolean): fun(arg: any): boolean
 function M.none(...)
   local validators = { ... }
   return function(value)
@@ -52,6 +43,7 @@ function M.none(...)
   end
 end
 
+---@type fun(...: fun(arg: any): boolean): fun(arg: any): boolean
 function M.any(...)
   local validators = { ... }
   return function(value)
@@ -64,7 +56,8 @@ function M.any(...)
   end
 end
 
-function M.all(...)
+---@type fun(...: fun(arg: any): boolean): fun(arg: any): boolean
+local function all(...)
   local validators = { ... }
 
   return function(value)
@@ -77,5 +70,9 @@ function M.all(...)
     return true
   end
 end
+
+M.all = all
+
+M.compose = all
 
 return M
